b71fb9675d56fcfdd577feded72071c94929ac9f
zucav <zucav@live.com>
2018-08-26 19:22:20 -0500

    update

--- classes/game.py
+++ classes/game.py
@@ -1,6 +1,5 @@
 import random
 
-
 class Bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -13,16 +12,15 @@ class Bcolors:
 
 
 class Person:
-    def __init__(self, hp, mp, atk, df, magic):
+    def __init__(self, hp, mp, atk, magic):
         self.maxhp = hp
         self.hp = hp
         self.maxmp = mp
         self.mp = mp
         self.atkl = atk - 10
         self.atkh = atk + 10
-        self.df = df
         self.magic = magic
-        self.actions = ["Attack", "Magic", "Defend"]
+        self.actions = ["Attack", "Magic"]
 
     def generate_damage(self):
         return random.randrange(self.atkl, self.atkh)
@@ -72,12 +70,3 @@ class Person:
         for spell in self.magic:
             print(str(i) + ":", spell["name"], "(cost:", str(spell["cost"]) + ")")
             i += 1
-
-    def defending_stance(self, dmg):
-        print(Bcolors.OKBLUE + "Defending" + Bcolors.ENDC)
-        if dmg > 20:
-            dmg = 20
-        self.hp -= dmg
-        if self.hp < 0:
-            self.hp = 0
-        return self.hp
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from classes.game import Person, Bcolors
 
 
-magic = [{"name": "Fire", "cost": 10, "dmg": 60},
-         {"name": "Thunder", "cost": 10, "dmg": 80},
-         {"name": "Blizzard", "cost": 10, "dmg": 60}]
+magic = [{"name": "Fire", "cost": 10, "dmg": 100},
+         {"name": "Thunder", "cost": 10, "dmg": 124},
+         {"name": "Blizzard", "cost": 10, "dmg": 100}]
 
-player = Person(460, 65, 60, 34, magic)
-enemy = Person(1200, 65, 45, 35, magic)
+player = Person(460, 65, 34, magic)
+enemy = Person(1200, 65, 35, magic)
 
 running = True
 i = 0
@@ -23,28 +23,57 @@ while running:
     if index == 0:
         dmg = player.generate_damage()
         enemy.take_damage(dmg)
-        print(Bcolors.UNDERLINE + "You attacked for " + str(dmg), "points of damage. Enemy HP:", str(enemy.get_hp()) + Bcolors.ENDC)
+        print("You attacked for " + str(dmg), "points of damage.")
+    elif index == 1:
+        print("=======Spells=========")
+        player.choose_magic()
+        magic_choice = int(input("Choose a Spell: ")) - 1
+        magic_dmg = player.generate_spell_damage(magic_choice)
+        spell = player.get_spell_name(magic_choice)
+        cost = player.get_spell_mp_cost(magic_choice)
 
+        current_mp = player.get_mp()
+
+        if cost > current_mp:
+            print(Bcolors.FAIL + "\nNot enough MP.\n" + Bcolors.ENDC)
+            continue
+
+        player.reduce_mp(cost)
+        enemy.take_damage(magic_dmg)
+        print(Bcolors.OKBLUE + "\n" + spell + " deals", str(magic_dmg), "points of damage." + Bcolors.ENDC)
 
     enemy_choice = 1
 
     enemy_dmg = enemy.generate_damage()
     player.take_damage(enemy_dmg)
-    print(Bcolors.UNDERLINE + "Enemy attacks for", str(enemy_dmg), "Player HP:", str(player.get_hp()) + Bcolors.ENDC)
+    print("Enemy attacks for", str(enemy_dmg))
+
+    print("----------------")
+    print("Enemy HP:", Bcolors.FAIL + str(enemy.get_hp()) + "/" + str(enemy.get_maxhp()) + Bcolors.ENDC, "\n")
+
+    print("Your HP:", Bcolors.OKGREEN + str(player.get_hp()) + "/" + str(player.get_maxhp()) + Bcolors.ENDC)
+    print("Your MP:", Bcolors.OKBLUE + str(player.get_mp()) + "/" + str(player.get_maxmp()) + Bcolors.ENDC, "\n")
 
 
-#    running = False
+
+
+##End Game
+    if enemy.get_hp() == 0:
+        print(Bcolors.OKGREEN + "You Win!" + Bcolors.ENDC)
+        running = False
+    elif player.get_hp() == 0:
+        print(Bcolors.FAIL + "You Lose!" + Bcolors.ENDC)
+        running = False
 
 
 
 
-'''
-    elif index == 1:
-        print("=======Spells=========")
-        player.choose_magic()
-        choice = input("Choose a Spell: ")
-        index = int(choice) - 1
-'''
+
+
+
+
+
+
 
 
 
