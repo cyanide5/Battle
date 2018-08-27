9607e9d28d3c3fa8cbf03cdf83dabc088ee453ac
zucav <zucav@live.com>
2018-08-26 20:46:27 -0500

    added inventory

---
+++ classes/inventory.py
@@ -0,0 +1,8 @@
+
+
+class Item:
+    def __init__(self, name, type, description, prop):
+        self.name = name
+        self.type = type
+        self.desctiption = description
+        self.prop = prop
